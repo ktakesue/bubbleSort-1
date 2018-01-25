00ed803026e60204cc1d61b9b56508be550e7a4f
ktakesue <ktakesue@aol.com>
2018-01-24 21:28:00 -1000

    passes first three tests

--- index.js
+++ index.js
@@ -2,7 +2,31 @@
 //'sort' will sort an array of values in the following sequence
 // null,0,1,2,3,....9999,Infinity,'A','AA','B','BA',...'a','aa','b','bb','c','d',....,'z',...,'zzzzz'
 //----if sort is given null it will return null
+let array = [];
 
+// make a helper function to swap i and j in the array //
+function swap(array, i, j){
+    var arr = array[i];
+    array[i] = array[j];
+    array[j] = arr;
+}
+
+function sort(array){
+ if (array === null){
+     return null;
+ }
+ // loop through array //       
+    for (var i = 0; i < array.length; i++){
+        for(var j = 1; j < array.length; j++){
+            if(array[j - 1] > array[j]){
+                swap(array, j - 1, j);
+            }
+        }
+    }
+    return array;
+}
+
+console.log(sort(array));
 
 
 
@@ -13,5 +37,5 @@
 
 /////////do not change below
 module.exports = {
-    sort
+    sort : sort
 }
